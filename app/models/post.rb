e49f392c525ef66f25bd85f96f98b58cd1dd9d14
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-12-26 11:46:07 +0800

    Fix quoted_table_name

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :votes
 
   scope :by_date, order("created_at DESC")
-  scope :by_team, lambda { |team| joins(:user).joins("INNER JOIN `teams_users` ON `teams_users`.user_id = `users`.id").where(["`teams_users`.team_id = ?", team.id])}
+  scope :by_team, lambda { |team| joins(:user).joins("INNER JOIN #{TeamsUser.quoted_table_name} ON #{TeamsUser.quoted_table_name}.user_id = #{User.quoted_table_name}.id").where(["#{TeamsUser.quoted_table_name}.team_id = ?", team.id])}
 
 def date
   self.created_at.to_date
